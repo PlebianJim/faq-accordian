6fa6deaf10eb83d4e5fd803adceb3506072811ba
PlebianJim <108274011+PlebianJim@users.noreply.github.com>
2023-12-04 09:54:35 +0000

    added text for faq's

---
+++ main.js
@@ -0,0 +1,17 @@
+function expandAccordian() {
+    var acc = document.getElementsByClassName('accordian');
+    var i;
+
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+}
