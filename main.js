913fe951d211ab635c7725e6e9de311e637b7e0b
PlebianJim <108274011+PlebianJim@users.noreply.github.com>
2023-12-04 11:12:18 +0000

    updated code to change plus icon to minus icon

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 function expandAccordian() {
-    var acc = document.getElementsByClassName('accordian');
-    var i;
+    let acc = document.getElementsByClassName("accordian");
+    let i;
 
     for (let i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
+        acc[i].addEventListener("click", function () {
+            let panel = this.nextElementSibling;
+            let icon = this.querySelector(".plusIcon");
 
-            var panel = this.nextElementSibling;
             if (panel.style.display === "block") {
                 panel.style.display = "none";
+                icon.src = "assets/images/icon-plus.svg";
             } else {
+                //Close any other open panels
+                let openPanels = document.querySelectorAll(".panel");
+                openPanels.forEach(function (openPanel) {
+                    openPanel.style.display = "none";
+                });
+
+                //Reset all icons to plus icon
+                let icons = document.querySelectorAll(".plusIcon");
+                icons.forEach(function (icon) {
+                    icon.src = "assets/images/icon-plus.svg";
+                });
+
                 panel.style.display = "block";
+                icon.src = "assets/images/icon-minus.svg";
             }
         });
     }
